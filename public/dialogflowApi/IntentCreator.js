60d8aaa0eeb7f608538b8d4e31a3308a3030a3b1
Michał Wejwoda <mwejwoda@sii.pl>
2020-01-07 14:06:21 +0100

    creating intents for pizzeria

--- public/dialogflowApi/IntentCreator.js
+++ public/dialogflowApi/IntentCreator.js
@@ -2,8 +2,67 @@ const dialogflow = require('dialogflow');
 
 class IntentCreator {
 
-    createPizzaTypeIntent() {
+    createDeliveryPhoneNumberIntent(deliveryTime) {
+        var trainingPhrases = [{
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'My number is '},
+                {text: '123123123', entityType: '@sys.phone-number', alias: 'phone'},
+            ],
+        },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: '58 342 23 12', entityType: '@sys.phone-number', alias: 'phone'},
+                ],
+            }
+        ];
+        const responseText = {
+            text: ['Ok, the order has been accepted. Thank you. Estimated delivery time is ' + deliveryTime],
+        };
+        const parameters = [{
+            prompts: [],
+            displayName: 'phone',
+            value: '$phone',
+            defaultValue: '',
+            entityTypeDisplayName: '@sys.phone-number',
+            mandatory: false,
+            isList: false
+        }];
+        this.createIntent(trainingPhrases, parameters, responseText, 'delivery phone number');
+    }
 
+    createDeliveryAddressIntent() {
+        var trainingPhrases = [{
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'My address is'},
+                {text: 'Skłodowskiej 25/34 Gdańsk', entityType: '@sys.address', alias: 'address'},
+            ],
+        },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'Beniowskiego 60/62 m 10 Toruń', entityType: '@sys.address', alias: 'address'},
+                ],
+            }
+        ];
+        const responseText = {
+            text: ['Ok, give us your phone number'],
+        };
+        const parameters = [{
+                 prompts: [],
+                 displayName: 'address',
+                 value: '$address',
+                 defaultValue: '',
+                 entityTypeDisplayName: '@sys.location',
+                 mandatory: false,
+                 isList: false
+        }];
+        this.createIntent(trainingPhrases, parameters, responseText, 'delivery address');
+    }
+
+    createPizzaTypeIntent() {
         var trainingPhrases = [{
             type: 'EXAMPLE',
             parts: [
@@ -25,12 +84,12 @@ class IntentCreator {
         };
         const parameters = [
             {
-                prompts: [],
+                prompts: ['Choose correct pizza'],
                 displayName: 'type',
                 value: '$type',
                 defaultValue: '',
                 entityTypeDisplayName: '@pizzaType',
-                mandatory: false,
+                mandatory: true,
                 isList: false
             },
         ];
@@ -52,8 +111,219 @@ class IntentCreator {
         //     mandatory: false,
         //     isList: false
         // }
-        const displayName = 'pizzaType';
+        const displayName = 'pizza type';
         this.createIntent(trainingPhrases, parameters, responseText, displayName);
+    }
+
+    createSauceIntent() {
+        var trainingPhrases = [
+            {
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'please '},
+                {text: 'tomato', entityType: '@pizzaSauce', alias: 'sauce'},
+                {text: ' sauce'}
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'I want '},
+                    {text: 'garlic', entityType: '@pizzaSauce', alias: 'sauce'},
+                    {text: ' sauce'}
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'garlic', entityType: '@pizzaSauce', alias: 'sauce'},
+                    {text: ' sauce'}
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'tomato', entityType: '@pizzaSauce', alias: 'sauce'},
+                ],
+            },
+        ];
+        const responseText = {
+            text: ['Ok, Please enter delivery address'],
+        };
+        const parameters = [
+            {
+                prompts: [],
+                displayName: 'sauce',
+                value: '$sauce',
+                defaultValue: '',
+                entityTypeDisplayName: '@pizzaSauce',
+                mandatory: false,
+                isList: false
+            },
+        ];
+        this.createIntent(trainingPhrases, parameters, responseText, 'pizza sauce');
+    }
+
+    createPizzaSizeIntent() {
+        var trainingPhrases = [{
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'I would like '},
+                {text: 'small', entityType: '@pizzaSize', alias: 'size'},
+            ],
+        },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'I want '},
+                    {text: 'medium', entityType: '@pizzaSize', alias: 'size'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'large', entityType: '@pizzaSize', alias: 'size'},
+                ],
+            }
+        ];
+        const responseText = {
+            text: ['Ok, What sauce to pizza?'],
+        };
+        const parameters = [
+            {
+                prompts: [],
+                displayName: 'size',
+                value: '$size',
+                defaultValue: '',
+                entityTypeDisplayName: '@pizzaSize',
+                mandatory: false,
+                isList: false
+            },
+        ];
+        this.createIntent(trainingPhrases, parameters, responseText, 'pizza size');
+    }
+
+    createPizzaDoughIntent() {
+        var trainingPhrases = [{
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'I would like '},
+                {text: 'thick', entityType: '@pizzaCrust', alias: 'dough'},
+            ],
+        },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'I want '},
+                    {text: 'thin', entityType: '@pizzaCrust', alias: 'dough'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'thin', entityType: '@pizzaCrust', alias: 'dough'},
+                ],
+            }
+        ];
+        const responseText = {
+            text: ['Ok, What should be pizza size?'],
+        };
+        const parameters = [
+            {
+                prompts: [],
+                displayName: 'dough',
+                value: '$dough',
+                defaultValue: '',
+                entityTypeDisplayName: '@pizzaCrust',
+                mandatory: false,
+                isList: false
+            },
+        ];
+        this.createIntent(trainingPhrases, parameters, responseText, 'pizza dough');
+    }
+
+    createPizzeriaAddressIntent(address) {
+        var trainingPhrases = [
+            {
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'What is the address of the pizzeria'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'Where is the pizzeria'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'Give me the address of the pizzeria'},
+                ],
+            }
+
+        ];
+
+        const responseText = {
+            text: ['Pizzeria address is ' + address],
+        };
+
+        this.createIntent(trainingPhrases, [], responseText, 'pizzeria address');
+    }
+
+    createPizzeriaPhoneNumberIntent(phoneNumber) {
+        var trainingPhrases = [
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'Give me phone number'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'What is phone number?'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'Give me the phone number to pizzeria'},
+                ],
+            },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'What is the phone number to pizzeria'},
+                ],
+            }
+
+        ];
+        const responseText = {
+            text: ['Pizzeria phone number is ' + phoneNumber],
+        };
+        this.createIntent(trainingPhrases, [], responseText, 'pizzeria phone');
+    }
+
+    createDeliveryCostIntent(deliveryCost) {
+        var trainingPhrases = [{
+            type: 'EXAMPLE',
+            parts: [
+                {text: 'What is the delivery cost'},
+            ],
+        },
+            {
+                type: 'EXAMPLE',
+                parts: [
+                    {text: 'give me the delivery cost'},
+                ],
+            }
+        ];
+        const responseText = {
+            text: ['Delivery cost is ' + deliveryCost],
+        };
+        this.createIntent(trainingPhrases, [], responseText, 'delivery cost');
     }
 
     createIntent(trainingPhrases, parameters, responseText, displayName) {
--- routes/createpizzeria.js
+++ routes/createpizzeria.js
@@ -1,9 +1,7 @@
-const dialogflow = require('dialogflow');
 var express = require('express');
 var router = express.Router();
 const EntityCreator = require('../public/dialogflowApi/EntityCreator');
 const IntentCreator = require('../public/dialogflowApi/IntentCreator');
-const EntityTypesCreator = require('../public/dialogflowApi/EntityTypesCreator');
 const IntentUpdater = require('../public/dialogflowApi/IntentUpdater');
 
 router.post('/', function(req, res, next) {
@@ -12,7 +10,6 @@ router.post('/', function(req, res, next) {
 
     var Creator = new EntityCreator();
     var ICreator = new IntentCreator();
-    var ETCreator = new EntityTypesCreator();
     var IUpdater = new IntentUpdater();
 
     var pizzasArray = req.body.pizzaNames.split(',');
@@ -24,12 +21,120 @@ router.post('/', function(req, res, next) {
     console.log(defaultResponse);
     IUpdater.updateDefaultIntent(defaultResponse);
 
-    //createEntities();
-    //wait();
-    //console.log('wait');
-    //createIntents();
-
+    createEntities(req.body);
+    wait();
+    console.log('wait');
+    createIntents(req.body);
     res.send('respond with a resource');
+
+    function createEntities(form)
+    {
+        createSizeEntity(form.sizes);
+        createPizzaEntity(pizzasArray);
+        createSaucesEntity(form.sauces);
+        createCrustEntity(form.dough);
+    }
+
+    function createIntents(form) {
+        ICreator.createPizzaTypeIntent();
+        ICreator.createPizzaDoughIntent();
+        ICreator.createPizzaSizeIntent();
+        ICreator.createSauceIntent();
+        ICreator.createDeliveryAddressIntent();
+        ICreator.createDeliveryPhoneNumberIntent(form.deliveryTime);
+        ICreator.createPizzeriaAddressIntent(form.address);
+        ICreator.createPizzeriaPhoneNumberIntent(form.phone);
+        ICreator.createDeliveryCostIntent(form.deliveryCost);
+    }
+
+    function createSizeEntity(selectedSizes) {
+
+        var sizes = [];
+        if (selectedSizes.includes('small')) {
+            sizes.push({
+                value: 'small',
+                synonyms: ['little', 'minor', 'tiny']
+            })
+        }
+        if (selectedSizes.includes('medium')) {
+            sizes.push({
+                value: 'medium',
+                synonyms: ['average', 'middle-sized', 'medium-sized']
+            })
+        }
+        if (selectedSizes.includes('large')) {
+            sizes.push({
+                value: 'large',
+                synonyms: ['big']
+            })
+        }
+        Creator.addValuesToEntity('pizzaSize', sizes);
+    }
+
+    function createPizzaEntity(pizzasToMenu) {
+
+        var pizzas = [];
+        pizzasToMenu.forEach(function(pizza){
+            pizzas.push({
+                value: pizza,
+                synonyms: [pizza]
+            })
+        });
+        Creator.addValuesToEntity('pizzaType', pizzas);
+    }
+
+    function createSaucesEntity(selectedSauces) {
+
+        var sauces = [];
+        if (selectedSauces.includes('tomato')) {
+            sauces.push({
+                value: 'tomato',
+                synonyms: ['tomato sauce']
+            })
+        }
+        if (selectedSauces.includes('garlic')) {
+            sauces.push({
+                value: 'garlic',
+                synonyms: ['garlic sauce']
+            })
+        }
+        if (selectedSauces.includes('dill')) {
+            sauces.push({
+                value: 'dill',
+                synonyms: ['dill sauce']
+            })
+        }
+        Creator.addValuesToEntity('pizzaSauce', sauces);
+    }
+
+    function createCrustEntity(selectedCrusts) {
+
+        var crust = [];
+        if (selectedCrusts.includes('thin')) {
+            crust.push({
+                value: 'thin',
+                synonyms: ['on thin', 'slim']
+            })
+        }
+        if (selectedCrusts.includes('normal')) {
+            crust.push({
+                value: 'normal',
+                synonyms: ['normal', 'mean']
+            })
+        }
+        if (selectedCrusts.includes('thick')) {
+            crust.push({
+                value: 'thick',
+                synonyms: ['big', 'large']
+            })
+        }
+        Creator.addValuesToEntity('pizzaCrust', crust);
+    }
+
+    async function wait()
+    {
+        await new Promise(resolve => {setTimeout(resolve, 5000)});
+    }
 });
 
 module.exports = router;
